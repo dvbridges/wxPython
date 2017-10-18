1e75319e0346ceb8bcc1665347cfafd70e64366f
David Bridges <david-bridges@hotmail.co.uk>
2017-10-18 19:24:56 +0100

    Initial commit

---
+++ saving_usr_input.py
@@ -0,0 +1,66 @@
+#! usr/bin/env python
+
+# define class
+# set gui function
+# create panel
+# create menuBar
+# ccreate buttons/menus
+# append items to buttons/menus
+# append menus to menubar
+# set text panel
+# bind functions or events to menu items
+# set menu bar
+# maximize and show
+# Define quit box
+# main()
+
+import wx
+import csv
+
+class windowClass(wx.Frame):
+
+	def __init__(self, *args, **kwargs):
+		super(windowClass, self).__init__(*args, **kwargs)
+
+		self.basicGUI()
+
+	def basicGUI(self):
+
+		#MENUBAR
+		menuBar = wx.MenuBar()
+		fileButton = wx.Menu()
+		editButton = wx.Menu()
+		exitItem = fileButton.Append(wx.ID_EXIT, 'Quit', 'Status: Closing window...')
+		menuBar.Append(fileButton, 'File')
+		menuBar.Append(editButton, 'Edit')
+		self.Bind(wx.EVT_MENU, self.Quit, exitItem)
+		self.SetMenuBar(menuBar)
+		# PANEL
+		panel = wx.Panel(self) 
+		write = wx.TextCtrl(panel, pos= (50,50), size = (300,300), style = wx.TE_MULTILINE)
+		# DIALOGUES
+		nameBox = wx.TextEntryDialog(None, 'What is your name?', 'Welcome', 'name')
+		yesNoBox = wx.MessageDialog(None, 'Do you enjoy wxPython?', 'Question', wx.YES_NO)
+		yesNoAnswer = yesNoBox.ShowModal()
+		yesNoBox.Destroy()
+
+		if nameBox.ShowModal() == wx.ID_OK:
+			userName = nameBox.GetValue()
+		if yesNoAnswer == wx.ID_NO:
+			userName = 'Loser'
+			write.AppendText('Losers do not enjoy wxPython!')
+		# SHOW
+		self.SetTitle('Welcome '+userName)
+		self.Show()
+
+	def Quit(self, event):
+		self.Close()
+
+
+def main():
+	app = wx.App()
+	windowClass(None, title = "New Window")
+	app.MainLoop()
+
+if __name__ == '__main__':
+	main()
